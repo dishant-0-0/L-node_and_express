3f1d101ae1a2654db2e180655d09bb4e718af7aa
dishant-0-0 <dishantpatel098@gmail.com>
2021-05-13 09:45:56 +0530

    update

--- T-testing.js
+++ T-testing.js
@@ -1,9 +1,32 @@
-const http =require('http')
+const {readFile}=require('fs');
+const util = require('util');
+const readFilePromise = util.promisify(readFile)
 
-const server = http.createServer((req, res)=>{
+const getText = (path)=>{
+    return new Promise((resolve,reject)=>{
+        readFile(path,'utf8',(err,data)=>{
+            if(err){
+                console.log(err);
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
+}
 
-})
+// getText('./T-demo-folder/first.txt')
+//     .then((result)=>console.log((result))
+//     .catch((err)=>console.log(err)))
 
-server.listen(5000,()=>{
-    console.log('Server is listening on Port 5000');
-})
+const start = async()=>{
+    try{
+        const first = await getText('./T-demo-folder/first.txt')
+        const second = await getText('./T-demo-folder/second.txt')
+        console.log(first,second);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+start()
